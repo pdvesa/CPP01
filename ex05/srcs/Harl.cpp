36163b7796ca0e033a93db2c085d6a0edd5c8dc5
pdvesa <saku.vesa@gmail.com>
2024-09-03 23:17:39 +0300

    tiny

--- ex05/srcs/Harl.cpp
+++ ex05/srcs/Harl.cpp
@@ -1,18 +1,22 @@
 #include <Harl.hpp>
 
 void Harl::debug(void) {
+    std::cout << "[ DEBUG ]" << std::endl;
     std::cout << "I hate debugging" << std::endl;
 }
 
 void Harl::info(void) {
+    std::cout << "[ INFO ]" << std::endl;
     std::cout << "I hate info even more" << std::endl;
 }
 
 void Harl::warning(void) {
+    std::cout << "[ WARNING ]" << std::endl;
     std::cout << "I hate warnings the most" << std::endl;
 }
 
 void Harl::error(void) {
+    std::cout << "[ ERROR ]" << std::endl;
     std::cout << "I can't even comprehend how much I hate errors" << std::endl;
 }
 
